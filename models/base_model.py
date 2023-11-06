b2110da4e4a5f8f9eecdea52f3c6bb7ec1b93bd2
aidan <aidanheydenrycht@gmail.com>
2023-11-06 14:59:16 +0200

    [Created] Python class BaseModel2

---
+++ models/base_model.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+
+This model contains a class BaseModel
+
+"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """class BaseModel"""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a class BaseModel instance"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+
+                if key in ('created_at', 'updated_at'):
+                    setattr(self, key, datetime.fromisoformat(value))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+
+    def __str__(self):
+        """Returns a string representation"""
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+
+    def save(self):
+        """Updates the current modified time"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dict containing all key/values of __dict__ of the instance"""
+        instance_dict = self.__dict__.copy()
+        instance_dict['__class__'] = self.__class__.__name__
+        instance_dict['created_at'] = self.created_at.isoformat()
+        instance_dict['updated_at'] = self.updated_at.isoformat()
+
+        return instance_dict
